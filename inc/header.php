4dd81f71ad09fe2514b7f9909cd6a42375fa6609
AntonyOnScript <santhiagoafk@gmail.com>
2021-06-19 12:17:40 -0300

    Added menu

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>InstaFonts</title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/<?php echo $pageStyle ?>.css">
@@ -11,6 +12,10 @@
     <script src="assets/js/<?php echo $pageScript ?>.js" defer></script>
 </head>
 <body>
-    <header>
-    
+    <header class="container-fluid d-flex justify-content-between align-items-center">
+        <a href="#"><h3 class="logo">InstaFonts</h3></a>
+        <nav class="nav">
+            <a href="#" class="nav-link link">Fonts</a>
+            <a href="#" class="nav-link link" >Emojis</a>
+        </nav>
     </header>
