6cc19b4c1458f224093e1f09152448738739c95b
AntonyOnScript <santhiagoafk@gmail.com>
2021-06-20 15:50:28 -0300

    Added About section

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "inc/header.php";
 
 ?>
 
-<section class="main container-fluid flex-collumn align-items-center">
+<section class="container-fluid main flex-column align-items-center">
     <div class="container main-cont">
         <div class="row large-logo-cont">
             <div class="col d-flex justify-content-center align-items-end large-logo-field">
@@ -14,7 +14,7 @@ require_once "inc/header.php";
         </div>
         <div class="row font-field">
             <div class="row">
-                <div class="col">
+                <div class="col sm-4">
                     <input type="text" class="form-control typed-text" placeholder="Your phrase, nickname or caption here.">
                 </div>
                 <div class="col-4">
@@ -37,8 +37,27 @@ require_once "inc/header.php";
     </div>
 </section>
 
-<section>
-
+<section class="container-fluid about d-flex flex-column align-items-center">
+    <div class="container">
+        <h1 class="text-center about-title">So... where can i use these custom fonts?</h1>
+        <p class="text-center about-paragraph description">Basically everywhere you want!</p>
+        <div class="places d-flex flex-column align-items-center">
+            <img src="assets/img/whatsapp.png" alt="">
+            <p class="text-center about-paragraph">On your whatsapp status or name!</p>
+        </div>
+        <div class="places d-flex flex-column align-items-center">
+            <img src="assets/img/instagram.png" alt="">
+            <p class="text-center about-paragraph">On your Instagram name, bio or photos' captions!</p>
+        </div>
+        <div class="places d-flex flex-column align-items-center">
+            <img src="assets/img/twitter.png" alt="">
+            <p class="text-center about-paragraph">On your twitter name, bio or posts!</p>
+        </div>
+        <div class="places d-flex flex-column align-items-center">
+            <img src="assets/img/assault-rifle.png" alt="">
+            <p class="text-center about-paragraph">On your favorite games, like PUBG, Free Fire, Mobile Legends and many others!</p>
+        </div>
+    </div>
 </section>
 
 <?php 
