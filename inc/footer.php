6bac9986d10c14d4b0f29df76b04e1a7a551b625
AntonyOnScript <santhiagoafk@gmail.com>
2021-06-20 16:07:49 -0300

    Added footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,7 @@
-<footer>
-        
+<footer class="container-fluid footer">
+    <div class="container">
+        <p class="text-end">Created by <a href="" class="link">Antony</a></p>
+    </div>
 </footer>
 
 </body>
