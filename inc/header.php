751306b94b68f62b65d076709064325a5a35797e
AntonyOnScript <santhiagoafk@gmail.com>
2021-06-20 16:18:02 -0300

    Separeted the .css files from header.php and footer.php

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
     <link rel="stylesheet" href="assets/css/<?php echo $pageStyle ?>.css">
     <script src="assets/js/bootstrap.js" defer></script>
     <script src="assets/js/<?php echo $pageScript ?>.js" defer></script>
