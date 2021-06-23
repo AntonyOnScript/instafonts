ab336b0d6a47a8f3249927666b2f3d7350c5d12b
AntonyOnScript <santhiagoafk@gmail.com>
2021-06-22 21:41:05 -0300

    Changed php extension to html

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="container-fluid footer">
-    <div class="container">
-        <p class="text-end">Created by <a href="" class="link">Antony</a></p>
-    </div>
-</footer>
-
-</body>
-</html>
